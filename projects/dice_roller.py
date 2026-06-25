c1e938643e011da065ac33ae06124587c458a038
Nitesh <niteshkjaiswal108@gmail.com>
2026-06-25 10:53:34 +0530

    Add dice roller program to simulate rolling a six-sided dice and display the result

---
+++ projects/dice_roller.py
@@ -0,0 +1,16 @@
+# Dice Roller
+#
+# Simulate rolling a six-sided dice.
+# Generate a random number between 1 and 6.
+# Display the result.
+#
+# Example:
+# Dice rolled: 4
+
+import random
+
+print('Welcome to Dice Roller')
+
+dice_rolled = random.randint(1, 6)
+
+print(f'Dice Rolled: {dice_rolled}')
