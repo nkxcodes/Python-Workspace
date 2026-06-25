b90751f225dd2623372a13bdc4631222f0192250
Nitesh <niteshkjaiswal108@gmail.com>
2026-06-25 10:49:28 +0530

    Add coin toss simulator program to randomly display Heads or Tails

---
+++ projects/dice_roller.py
@@ -0,0 +1,15 @@
+# Coin Toss Simulator
+#
+# Simulate flipping a coin.
+# Randomly display Heads or Tails.
+#
+# Example:
+# Result: Heads
+
+import random
+
+print('Welcome to Coin Toss Simulator')
+
+result = random.choice(['Heads', 'Tails'])
+
+print(f'Result: {result}')
