ad0f1949fd2350a3a516231a3d44b3d6c5180c4f
Nitesh <niteshkjaiswal108@gmail.com>
2026-06-26 17:27:35 +0530

    Add remove duplicates program to create a list of unique values from a given list

---
+++ projects/remove_duplicates.py
@@ -0,0 +1,25 @@
+# Remove Duplicates
+#
+# Create a list containing duplicate values.
+# Remove duplicates.
+# Display the new list.
+#
+# Example:
+# Original: [1, 2, 2, 3]
+# New: [1, 2, 3]
+
+original_list = [1, 2, 2, 3]
+num = []
+
+# for num in range(0, len(original_list)):
+#     for num_1 in range(1, len(original_list)):
+#         if original_list[num] == original_list[num_1]:
+
+# for index in range(0, len(original_list) + 1):
+#         if num[index] == original_list[index]:
+#             original_list.pop(index)
+#         else:
+#             num.append(original_list[index])
+
+print(original_list)
+print(num)
