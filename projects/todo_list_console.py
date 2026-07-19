f380f8c214ad42b85c937f3ded19368add9008ce
Nitesh <niteshkjaiswal108@gmail.com>
2026-07-19 17:15:45 +0530

    Add console-based todo list program to manage tasks with options to add, view, and remove tasks

---
+++ projects/todo_list_console.py
@@ -0,0 +1,47 @@
+
+
+is_running_outer = True
+
+tasks = []
+
+
+
+while is_running_outer:
+    print('===== TO DO LIST =====')
+    print('1. Add Task')
+    print('2. View Tasks')
+    print('3. Remove Task')
+    print('4. Exit')
+
+    choosed = int(input('Choose: '))
+
+    if choosed == 1:
+        is_running = True
+        print('Enter Done to end the program: ')
+        while is_running:
+            task = input('Enter a Task: ')
+            if task == 'done' or task == 'Done':
+                break
+            tasks.append(task)
+        print(tasks)
+        print('')
+    elif choosed == 2:
+        print('')
+        if len(tasks) == 0:
+            print('No Tasks Available.')
+        else:
+            for index, task in enumerate(tasks, start=1):
+                print(f'{index}. {task}')
+        print('')
+    elif choosed == 3:
+        if len(tasks) == 0:
+            print('No Tasks Available.')
+        else:
+            for index, task in enumerate(tasks, start=1):
+                print(f'{index}. {task}')
+        task_to_remove = int(input('Enter task number to remove: '))
+        tasks.pop(task_to_remove - 1)
+        print('')
+    elif choosed == 4:
+        is_running_outer = False
+        print('Thank You! See you soon')
